746057a253ddf552a8d53ce190c6d29c31221519
shunnarski <ashunnarah9597@gmail.com>
2018-02-06 14:14:35 -0600

    Implemented the Get_Sequences function

--- Project1/project1_functions.py
+++ Project1/project1_functions.py
@@ -1,5 +1,5 @@
 def Get_Sequences(file_1, file_2):
-	"""
+    """
 	Reads sequences from the two input files
 
 	Args:
@@ -10,10 +10,40 @@ def Get_Sequences(file_1, file_2):
 		Returns the two sequences in a tuple of the form (str, str)
 
 	"""
-	pass
-	
+
+    file1 = open(file_1, "r")
+    file2 = open(file_2, "r")
+
+    # get only the sequence letters, initially this will be a list of different sets
+    # of letters due to the line breaks in the .txt file
+    with file1 as f:
+        human = f.read().splitlines()[1::]
+
+    with file2 as f:
+        mouse = f.read().splitlines()[1::]
+
+    file1.close()
+    file2.close()
+
+    # concatenate the strings of hemoglobin codes to one sequence
+    human_sequence = ""
+    mouse_sequence = ""
+
+    for s in human:
+        human_sequence += s
+
+    for s in mouse:
+        mouse_sequence += s
+
+    # set the human and mouse sequences to a tuple called sequences
+    sequences = (human_sequence, mouse_sequence)
+
+    # return the tuple of sequences
+    return sequences
+
+
 def Build_Scoring_Matrix(file_1):
-	"""
+    """
 	Builds the BLOSUM scoring matrix from the input file
 
 	Args:
@@ -23,10 +53,10 @@ def Build_Scoring_Matrix(file_1):
 		Returns a dictionary of the form BLOSUM_dict[(str, str)] = int
 
 	"""
-	pass
-	
+
+
 def Smith_Waterman(sequence_1, sequence_2, scoring_matrix):
-	"""
+    """
 	Smith-Waterman algorithm
 
 	Args:
@@ -37,10 +67,11 @@ def Smith_Waterman(sequence_1, sequence_2, scoring_matrix):
 	Returns:
 		Returns the two aligned sequences and the score in a tuple of the form (str, str, int)
 	"""
-	pass
+    pass
+
 
 def Needlman_Wunsch(sequence_1, sequence_2, scoring_matrix):
-	"""
+    """
 	Needlman-Wunsch algorithm
 
 	Args:
@@ -51,10 +82,11 @@ def Needlman_Wunsch(sequence_1, sequence_2, scoring_matrix):
 	Returns:
 		Returns the two aligned sequences and the score in a tuple of the form (int, str, str)
 	"""
-	pass
-	
+    pass
+
+
 def Output_Sequences(sequence_1, sequence_2, score):
-	"""
+    """
 	Prints (or writes to a file?) the score and two aligned sequences in the format specifed by the project documentation
 
 	Args:
@@ -63,4 +95,4 @@ def Output_Sequences(sequence_1, sequence_2, score):
 		score: the score of the aligned sequences
 
 	"""
-	pass
+    pass
