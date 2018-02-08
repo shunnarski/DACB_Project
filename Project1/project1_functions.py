960e724f38e706c837f0b0aede3ede82e3d7f5d0
shunnarski <ashunnarah9597@gmail.com>
2018-02-07 18:36:16 -0600

    Smith-Waterman algorithm implemented. Plan on asking DB more about it to make sure I did this right.

--- Project1/project1_functions.py
+++ Project1/project1_functions.py
@@ -55,7 +55,7 @@ def Build_Scoring_Matrix(file_1):
 	"""
     """
     1. Create list of proteins in matrix in order
-    2. Extract BLOSUM62 values in the matrix into a 2x2 int array
+    2. Extract BLOSUM62 values in the matrix into an int array
     3. Assign a tuple to each value in the matrix in a dictionary
     4. Return the dictionary
     """
@@ -98,7 +98,107 @@ def Smith_Waterman(sequence_1, sequence_2, scoring_matrix):
 	Returns:
 		Returns the two aligned sequences and the score in a tuple of the form (str, str, int)
 	"""
-    pass
+
+    # initialize scoring matrix
+    blosum_dict = scoring_matrix
+
+    # Build the matrix
+    w = len(sequence_1) + 1
+    h = len(sequence_2) + 1
+
+    def build_sw_matrix(w, h):
+        """
+        1. Initialize a matrix of size (len(sequence1) + 1) X (len(sequence2) + 1) with zeroes
+        2. For each cell past the first column and row calculate the score of that position in the matrix
+        using the blosum dictionary
+        3. Set that cell position with the calculated score
+        4. Return the matrix
+        """
+        # initialize matrix size with all zeroes
+        matrix = [[0 for col in range(w)] for row in range(h)]
+
+        for i in range(1, h):
+            for j in range(1, w):
+                score = calc_cell_val(matrix, i, j)
+                matrix[i][j] = score
+        return matrix
+
+    def get_start_pos(sw_matrix):
+        """
+        Look through the Smith-Waterman matrix to find the largest value and return the
+        position of the largest value
+        """
+        max_score = 0
+        max_pos = (0, 0)
+        for i in range(h):
+            for j in range(w):
+                val = sw_matrix[i][j]
+                if val > max_score:
+                    max_score = val
+                    max_pos = (i, j)
+        return max_pos
+
+    def calc_cell_val(matrix, i, j):
+        """
+        Calculate the score of the matrix cell by using the sum of its blosum value
+        and the cells above, left, and upper-left, then return the largest of these values
+        """
+
+        gap = -4  # gaps are typically -4 in the blosum matrix
+        dict_val = blosum_dict[(sequence_1[i - 1], sequence_2[j - 1])]
+        l = matrix[i - 1][j] + gap  # left cell
+        u = matrix[i][j - 1] + gap  # above cell
+        u_l = matrix[i - 1][j - 1] + dict_val  # upper-left cell
+
+        return max(l, u, u_l, 0)  # return largest of values. if 0 is biggest, return that
+
+    def backtrack(sw_matrix, start_pos):
+        """
+        Start the backtrack at the starting position, which is the cell in the Smith_Waterman matrix
+        that contains the largest value in the matrix. From there, compare the upper-left, upper, and
+        left cell values to determine the best possible path. Once the first 0 is hit in the backtracking
+        process, we're done. Return the two aligned sequences.
+        """
+        i, j = start_pos
+        align1 = ""
+        align2 = ""
+        while sw_matrix[i][j] != 0:  # once a cell is 0, backtarck ends
+            u_l = sw_matrix[i - 1][j - 1]  # upper-left cell
+            u = sw_matrix[i][j - 1]  # upper cell
+            l = sw_matrix[i - 1][j - 1]  # left cell
+            if u_l >= u and u_l >= l:  # choose the largest of the three
+                align1 += sequence_1[i - 1]  # append the character in the sequence to the aligned sequence
+                align2 += sequence_2[j - 1]
+                i -= 1  # decrement to move back up the matrix
+                j -= 1
+            elif u > u_l and u > l:
+                align1 += sequence_1[i - 1]
+                align2 += "*"  # represents a space
+                i -= 1
+            elif l > u_l and l > u:
+                align1 += "*"
+                align2 += sequence_2[j - 1]
+                j -= 1
+        align1 = align1[::-1]  # reverse the strings we made from backtracking. This will be our final alignment
+        align2 = align2[::-1]
+        return align1, align2
+
+    def calculate_alignment_score(align1, align2):
+        """
+        Take in the two alignment sequences and use the blosum62 dictionary to determine the score
+        of the two sequences. Not entirely sure if the starting position is the score of
+        our alignment or not...planning on asking DB.
+        """
+        score = 0
+        for i in range(len(align1)):
+            score += blosum_dict[(align1[i], align2[i])]
+        return score
+
+    sw_matrix = build_sw_matrix(w, h) # build matrix
+    start_pos = get_start_pos(sw_matrix)    # get start position, where the largest value in the Smith-Waterman matrix is located
+    align1, align2 = backtrack(sw_matrix, start_pos)    # get alignments
+    score = calculate_alignment_score(align1, align2)   # calculate score of the two alignments
+    return align1, align2, score    #return alignments and score
 
 
 def Needlman_Wunsch(sequence_1, sequence_2, scoring_matrix):
@@ -111,7 +211,7 @@ def Needlman_Wunsch(sequence_1, sequence_2, scoring_matrix):
 		scoring_matrix: The matrix to determine the score of alignments
 
 	Returns:
-		Returns the two aligned sequences and the score in a tuple of the form (int, str, str)
+		Returns the two aligned sequences and the score in a tuple of the form (str, str, int)
 	"""
     pass
 
