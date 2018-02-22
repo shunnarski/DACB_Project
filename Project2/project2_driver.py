5bc579f5b162472adb9c348988bcfccec5ac1f5b
jgaston93 <jgaston93@gmail.com>
2018-02-22 00:09:42 -0600

    Removed hardcoded file names
    Switched to using script arguments to read in data files for the sake of
    generality

--- Project2/project2_driver.py
+++ Project2/project2_driver.py
@@ -1,8 +1,13 @@
 import project2_functions as p2f
+import sys
 
-amino_acids = p2f.read_file("Proteins.fa")
+if len(sys.argv) != 3:
+    print "usage: project2_driver.py sequences_file_name labels_file_name"
+    sys.exit()
+
+amino_acids = p2f.read_file(sys.argv[1])
+amino_labels = p2f.read_file(sys.argv[2])
 amino_acids = p2f.extract_features(amino_acids)
-amino_labels = p2f.read_file("Proteins.sa")
 
 acid_train, acid_test, label_train, label_test = p2f.split_data(amino_acids, amino_labels)
 
