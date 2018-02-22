9ac1462a260ad82d296c7b7b960369d82d295a37
jgaston93 <jgaston93@gmail.com>
2018-02-22 00:13:47 -0600

    Update to ID3 implementation
    Added more comments
    Update:
    -Randomly break ties when two or more attributes provide the same
    information gain
    -Fixed bug of using wrong splits for creating the child nodes
    -Entropy function only takes one argument now

--- Project2/ID3.py
+++ Project2/ID3.py
@@ -1,10 +1,18 @@
 from Tree import Node
 import math
+import random
 
 def ID3(Attributes, X, Y):
+    """
+    Implementaion of the ID3 Algorithm
 
+    :param Attributes: List of attributes to test
+    :param X: set of training instances
+    :param Y: set of training labels
+    :return: decision tree build from the training data
+    """
     # Handle base cases
-    num_positive, num_negative = Calculate_Proportions(Y)
+    num_positive, num_negative = Calculate_Counts(Y)
     # All labels are positive
     if num_positive == len(Y):
         return Node(label='e')
@@ -15,59 +23,86 @@ def ID3(Attributes, X, Y):
     if len(Attributes) == 0:
         if num_positive > num_negative:
             return Node(label='e')
-        else:
+        elif num_positive < num_negative:
             return Node(label='-')
+        else:
+            return Node(label=random.choice(['e','-']))
 
     # Determine the best attribute
-    max_gain = 0
+    max_gain = None
     max_attribute = 0
-    max_index = 0
-    max_positive_split = []
-    max_negative_split = []
+    max_indices = []
+    positive_splits = []
+    negative_splits = []
     gains = []
     for i in range(len(Attributes)):
         gain, positive_split, negative_split = Gain(Attributes[i], X, Y)
         gains.append(gain)
-        if gain > max_gain:
+        positive_splits.append(positive_split)
+        negative_splits.append(negative_split)
+        # Keep track of max gain(s)
+        if max_gain == None:
             max_gain = gain
-            max_attribute = Attributes[i]
-            max_index = i
-            max_positive_split = positive_split
-            max_negative_split = negative_split
+            max_indices.append(i)
+        elif gain > max_gain:
+            max_gain = gain
+            max_indices = [i]
+        elif gain == max_gain:
+            max_indices.append(i)
+        
 
-    
+    max_index = random.choice(max_indices)
+    max_attribute = Attributes[max_index]
+    max_positive_split = positive_splits[max_index]
+    max_negative_split = negative_splits[max_index]
+
     # Remove the attribute from the list of attributes (Attrubutes - {A})
     Attributes = Attributes[:max_index] + Attributes[max_index+1:]
 
     # Set Attribute of the decision node to the one with the max gain
     current_node = Node(attribute=max_attribute)
 
-    if len(positive_split[0]) > 0:
-        current_node.Positive_Branch = ID3(Attributes, positive_split[0], positive_split[1])
+    # Build positive child node
+    if len(max_positive_split[0]) > 0:
+        current_node.Positive_Branch = ID3(Attributes, max_positive_split[0], max_positive_split[1])
     else:
-        num_positive, num_negative = Calculate_Proportions(positive_split[1])
-        if num_positive >= num_negative:
+        if max_positive_split[2] >= 0.5:
             current_node.Positive_Branch = Node(label='e')
         else:
             current_node.Positive_Branch = Node(label='-')
 
-    if len(negative_split[0]) > 0:
-        current_node.Negative_Branch = ID3(Attributes, negative_split[0], negative_split[1])
+    # Build negative child node
+    if len(max_negative_split[0]) > 0:
+        current_node.Negative_Branch = ID3(Attributes, max_negative_split[0], max_negative_split[1])
     else:
-        num_positive, num_negative = Calculate_Proportions(negative_split[1])
-        if num_positive >= num_negative:
+        if max_negative_split[2] >= 0.5:
             current_node.Negative_Branch = Node(label='e')
         else:
             current_node.Negative_Branch = Node(label='-')
     return current_node
 
-def H(proportion_pos, proportion_neg):
+def H(proportion_pos):
+    """
+    Calculates entropy based on the given proportions
+
+    :param proportion_pos: Proportion of positive training examples
+    :return: Returns the entropy based on the given proportion
+    """
     if proportion_pos == 0 or proportion_pos == 1:
         return 0
-    return -(proportion_pos*math.log(proportion_pos, 2)) - (proportion_neg*math.log(proportion_neg, 2))
+    return -(proportion_pos*math.log(proportion_pos, 2)) - ((1-proportion_pos)*math.log((1-proportion_pos), 2))
 
 def Attribute_Split(Attribute, X, Y):
     """
+    Splits the data on the given attribute and calculates the proportion 
+    of positive lables in the total set and both of the splits which is 
+    used later for calculating the entropy and information gain of these sets
+
+    :param Attribute: Attribute to perform split on
+    :param X: List of training instances
+    :param Y: List of training labels
+    :return: Two splits based on the given attribute along with proportions of positive examples
+                ((pos_X, pos_Y, proportion_pos), (neg_X, neg_Y, proportion_pos), total_proportion_pos)
     """
     # Initialize variables
     positive_X = []
@@ -96,29 +131,57 @@ def Attribute_Split(Attribute, X, Y):
             if Y[i] == 'e':
                 split2_num_pos += 1
             split2_num_total += 1
+
     # Calculate the proportions
     split1_proportion_pos = 0
     split2_proportion_pos = 0
+    total_proportion_pos = 0
+    denom = 0
+
+    # Proportion positive for each of the splits
     if split1_num_total > 0:
         split1_proportion_pos = split1_num_pos/float(split1_num_total)
+        denom += split1_num_total
     if split2_num_total > 0:
         split2_proportion_pos = split2_num_pos/float(split2_num_total)
-
-    return (positive_X, positive_Y), (negative_X, negative_Y), (split1_proportion_pos, split2_proportion_pos)
+        denom += split2_num_total
+    # Proportion positive for all of the instances
+    if denom > 0:
+        total_proportion_pos = (split1_num_pos + split2_num_pos)/float(denom)
+    
+    return (positive_X, positive_Y, split1_proportion_pos), (negative_X, negative_Y, split2_proportion_pos), total_proportion_pos
 
 def Gain(Attribute, X, Y):
-    pos_split, neg_split, proportions = Attribute_Split(Attribute, X, Y)
+    """
+    Calculates the information gain for the given attribute
+
+    :param Attribute: Attribute to test
+    :param X: List of training instances
+    :param Y: List of training labels
+    :return: Information gain for the given attribute
+    """
+    # Get splits and proportions
+    pos_split, neg_split, proportion_pos = Attribute_Split(Attribute, X, Y)
+
+    # Calculate weights based on split proportions
     num_pos_examples = len(pos_split[0])
     num_neg_examples = len(neg_split[0])
-    pos_split_weight = num_pos_examples/float(num_pos_examples + num_neg_examples)
-    neg_split_weight = num_neg_examples/float(num_pos_examples + num_neg_examples)
-    num_total_examples = num_pos_examples + num_neg_examples
-    num_pos_labels, num_neg_labels = Calculate_Proportions(Y)
-    proportion_pos_labels = num_pos_labels/float(num_pos_labels + num_neg_labels)
-    gain = H(proportion_pos_labels, 1 - proportion_pos_labels) - (pos_split_weight*H(proportions[0], 1 - proportions[0]) + neg_split_weight*H(proportions[1], 1 - proportions[1]))
+    num_total_examples = float(num_pos_examples + num_neg_examples)
+    pos_split_weight = num_pos_examples/num_total_examples
+    neg_split_weight = num_neg_examples/num_total_examples
+
+    # Finally calculate the gain
+    gain = H(proportion_pos) - (pos_split_weight*H(pos_split[2]) + neg_split_weight*H(neg_split[2]))
+    
     return gain, pos_split, neg_split
 
-def Calculate_Proportions(Y):
+def Calculate_Counts(Y):
+    """
+    Calculate the number of positive and negative labels
+
+    :param Y:  List of labels
+    :return: Counts of positive and negative labels
+    """
     num_positive = 0
     num_negative = 0
     for label in Y:
