dd579b691093cb3e1d5446248147782a737e8102
Cloud User <jgaston93@gmail.com>
2018-04-23 13:55:26 +0000

    Linear Regression works with 40 pssm features

--- Project5/LinearRegression.py
+++ Project5/LinearRegression.py
@@ -1,6 +1,6 @@
 import random
 import math
-from Data_Utils import Get_Data
+from Data_Utils import Get_Features
         
 def get_batch(X, Y, batch_size=32):
     batch_indices = random.sample(range(len(X)), batch_size)
@@ -8,11 +8,11 @@ def get_batch(X, Y, batch_size=32):
     batch_y = []
     index_counter = 0
     for index in batch_indices:
-        batch_x.append(X[index])
+        batch_x.append(Get_Features(X[index]))
         batch_y.append(Y[index])
     return batch_x, batch_y
 
-class LogisticRegression:
+class LinearRegression:
 
     def __init__(self, num_weights=201, max_iter=200, batch_size=64,
                     learning_rate=1e-3, reg_strength=1e-4, convergence_threshold=1e-4):
@@ -29,22 +29,28 @@ class LogisticRegression:
         return error
         
         
-    def fit(self, X, Y, verbose=False, log_filename="test.log"):
-        self._init_weights(len(X[0]))
+    def fit(self, X, Y, test_X, test_Y, verbose=False, log_filename="test.log"):
+        self._init_weights(40)
         acc_train_error = 0
+        acc_test_error = 0
         for t in range(self.max_iter):
-            if verbose and t % 5 == 0 and t != 0:
-                avg_train_error = acc_train_error/(self.batch_size*5.0)
+            if verbose and t % 100 == 0 and t != 0:
+                avg_train_error = acc_train_error/(self.batch_size*100.0)
+                avg_test_error = acc_test_error/(self.batch_size*100.0)
                 acc_train_error = 0
-                print "Train Error: {}".format(avg_train_error)
+                acc_test_error = 0
+                print "Train Error: {}\tTest Error: {}\n".format(avg_train_error, avg_test_error)
                 with open(log_filename, "a") as log_file:
-                    log_file.write("{},{}\n".format(t,avg_train_error))
+                    log_file.write("{},{},{}\n".format(t,avg_train_error,avg_test_error))
 
-            batch_X, batch_Y = get_batch(X, Y, batch_index)
+            batch_X, batch_Y = get_batch(X, Y, self.batch_size)
+            test_batch_X, test_batch_Y = get_batch(test_X, test_Y, self.batch_size)
 
             error = self.loss(batch_X, batch_Y)
+            test_error = self.loss(test_batch_X, test_batch_Y)
 
             acc_train_error += sum([e**2 for e in error])
+            acc_test_error += sum([e**2 for e in test_error])
 
             
             for i in range(len(batch_X[0])):
@@ -52,16 +58,14 @@ class LogisticRegression:
                 for j in range(len(batch_X)):
                     weight_update += batch_X[j][i]*error[j]
                 weight_update /= float(len(batch_X))
-                self.weights[i] -= self.learning_rate*(2*weight_update[i]) - 2*self.reg_strength*self.weights[i])
+                self.weights[i] += 2*self.learning_rate*(weight_update)
 
         if verbose:
-            avg_train_error = acc_train_error/(self.batch_size*5.0)
-            acc_train_error = 0
-            print "Train Error: {}".format(avg_train_error)
+            avg_train_error = acc_train_error/(self.batch_size*100.0)
+            avg_test_error = acc_test_error/(self.batch_size*100.0)
+            print "Train Error: {}\tTest Error: {}\n".format(avg_train_error, avg_test_error)
             with open(log_filename, "a") as log_file:
-                log_file.write("{},{}\n".format(t,avg_train_error))
-
-                 
+                 log_file.write("{},{},{}\n".format(t,avg_train_error,avg_test_error))                 
             
 
     def predict(self, X):
--- Project5/__init__.py
+++ Project5/__init__.py
@@ -1,15 +1,15 @@
 import Data_Utils
-from LogisticRegression import LogisticRegression
+from LinearRegression import LinearRegression
 
 
 if __name__ == "__main__":
     print "Loading Data ..."
-    X, Y = Data_Utils.Load_Data("data/", 5)
+    X, Y = Data_Utils.Load_Dataset("labels.csv")
     print "Splitting Data ..."
-    train_index, test_index = Data_Utils.Split_Data(len(X))
-    print "Total: {}".format(len(X))
-    print "Train split: {}".format(len(train_index))
-    print "Test split: {}".format(len(test_index))
-    clf = LogisticRegression(batch_size=64, max_iter=200, learning_rate=1e-3, reg_strength=1e-4, convergence_threshold=1e-4)
-    clf.fit(X, Y, train_index, test_index, verbose=True)
+    train_x, test_x, train_y, test_y = Data_Utils.Split_Data(X, Y)
+    print "Total: {}".format(len(train_x))
+    print "Train split: {}".format(len(train_x))
+    print "Test split: {}".format(len(test_x))
+    clf = LinearRegression(batch_size=64, max_iter=10000, learning_rate=1e-5, reg_strength=1e-4, convergence_threshold=1e-4)
+    clf.fit(train_x, train_y, test_x, test_y, verbose=True)
     clf.save_weights("test.mdl")
